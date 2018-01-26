1575cbfde8a87216c14a704ceea273d96d104799
Curtis Hard <curtishard@me.com>
2018-01-26 18:16:03 +0000

    Moved color tree over to modern syntax

--- source/IJSVGColor.m
+++ source/IJSVGColor.m
@@ -11,7 +11,7 @@
 
 @implementation IJSVGColor
 
-static NSMutableDictionary * _colorTree = nil;
+static NSDictionary * _colorTree = nil;
 
 CGFloat * IJSVGColorCSSHSLToHSB(CGFloat hue, CGFloat saturation, CGFloat lightness)
 {
@@ -56,157 +56,156 @@ CGFloat * IJSVGColorCSSHSLToHSB(CGFloat hue, CGFloat saturation, CGFloat lightne
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        
-        _colorTree = [[NSMutableDictionary alloc] init];
-        // add the colours in
-        [_colorTree setObject:@"f0f8ff" forKey:@"aliceblue"];
-        [_colorTree setObject:@"faebd7" forKey:@"antiquewhite"];
-        [_colorTree setObject:@"00ffff" forKey:@"aqua"];
-        [_colorTree setObject:@"7fffd4" forKey:@"aquamarine"];
-        [_colorTree setObject:@"f0ffff" forKey:@"azure"];
-        [_colorTree setObject:@"f5f5dc" forKey:@"beige"];
-        [_colorTree setObject:@"ffe4c4" forKey:@"bisque"];
-        [_colorTree setObject:@"000000" forKey:@"black"];
-        [_colorTree setObject:@"ffebcd" forKey:@"blanchedalmond"];
-        [_colorTree setObject:@"0000ff" forKey:@"blue"];
-        [_colorTree setObject:@"8a2be2" forKey:@"blueviolet"];
-        [_colorTree setObject:@"a52a2a" forKey:@"brown"];
-        [_colorTree setObject:@"deb887" forKey:@"burlywood"];
-        [_colorTree setObject:@"5f9ea0" forKey:@"cadetblue"];
-        [_colorTree setObject:@"7fff00" forKey:@"chartreuse"];
-        [_colorTree setObject:@"d2691e" forKey:@"chocolate"];
-        [_colorTree setObject:@"ff7f50" forKey:@"coral"];
-        [_colorTree setObject:@"6495ed" forKey:@"cornflowerblue"];
-        [_colorTree setObject:@"fff8dc" forKey:@"cornsilk"];
-        [_colorTree setObject:@"dc143c" forKey:@"crimson"];
-        [_colorTree setObject:@"000000" forKey:@"currentcolor"];
-        [_colorTree setObject:@"00ffff" forKey:@"cyan"];
-        [_colorTree setObject:@"00008b" forKey:@"darkblue"];
-        [_colorTree setObject:@"008b8b" forKey:@"darkcyan"];
-        [_colorTree setObject:@"b8860b" forKey:@"darkgoldenrod"];
-        [_colorTree setObject:@"a9a9a9" forKey:@"darkgray"];
-        [_colorTree setObject:@"006400" forKey:@"darkgreen"];
-        [_colorTree setObject:@"a9a9a9" forKey:@"darkgrey"];
-        [_colorTree setObject:@"bdb76b" forKey:@"darkkhaki"];
-        [_colorTree setObject:@"8b008b" forKey:@"darkmagenta"];
-        [_colorTree setObject:@"556b2f" forKey:@"darkolivegreen"];
-        [_colorTree setObject:@"ff8c00" forKey:@"darkorange"];
-        [_colorTree setObject:@"9932cc" forKey:@"darkorchid"];
-        [_colorTree setObject:@"8b0000" forKey:@"darkred"];
-        [_colorTree setObject:@"e9967a" forKey:@"darksalmon"];
-        [_colorTree setObject:@"8fbc8f" forKey:@"darkseagreen"];
-        [_colorTree setObject:@"483d8b" forKey:@"darkslateblue"];
-        [_colorTree setObject:@"2f4f4f" forKey:@"darkslategray"];
-        [_colorTree setObject:@"2f4f4f" forKey:@"darkslategrey"];
-        [_colorTree setObject:@"00ced1" forKey:@"darkturquoise"];
-        [_colorTree setObject:@"9400d3" forKey:@"darkviolet"];
-        [_colorTree setObject:@"ff1493" forKey:@"deeppink"];
-        [_colorTree setObject:@"00bfff" forKey:@"deepskyblue"];
-        [_colorTree setObject:@"696969" forKey:@"dimgray"];
-        [_colorTree setObject:@"696969" forKey:@"dimgrey"];
-        [_colorTree setObject:@"1e90ff" forKey:@"dodgerblue"];
-        [_colorTree setObject:@"b22222" forKey:@"firebrick"];
-        [_colorTree setObject:@"fffaf0" forKey:@"floralwhite"];
-        [_colorTree setObject:@"228b22" forKey:@"forestgreen"];
-        [_colorTree setObject:@"ff00ff" forKey:@"fuchsia"];
-        [_colorTree setObject:@"dcdcdc" forKey:@"gainsboro"];
-        [_colorTree setObject:@"f8f8ff" forKey:@"ghostwhite"];
-        [_colorTree setObject:@"ffd700" forKey:@"gold"];
-        [_colorTree setObject:@"daa520" forKey:@"goldenrod"];
-        [_colorTree setObject:@"808080" forKey:@"gray"];
-        [_colorTree setObject:@"008000" forKey:@"green"];
-        [_colorTree setObject:@"adff2f" forKey:@"greenyellow"];
-        [_colorTree setObject:@"808080" forKey:@"grey"];
-        [_colorTree setObject:@"f0fff0" forKey:@"honeydew"];
-        [_colorTree setObject:@"ff69b4" forKey:@"hotpink"];
-        [_colorTree setObject:@"cd5c5c" forKey:@"indianred"];
-        [_colorTree setObject:@"4b0082" forKey:@"indigo"];
-        [_colorTree setObject:@"fffff0" forKey:@"ivory"];
-        [_colorTree setObject:@"f0e68c" forKey:@"khaki"];
-        [_colorTree setObject:@"e6e6fa" forKey:@"lavender"];
-        [_colorTree setObject:@"fff0f5" forKey:@"lavenderblush"];
-        [_colorTree setObject:@"7cfc00" forKey:@"lawngreen"];
-        [_colorTree setObject:@"fffacd" forKey:@"lemonchiffon"];
-        [_colorTree setObject:@"add8e6" forKey:@"lightblue"];
-        [_colorTree setObject:@"f08080" forKey:@"lightcoral"];
-        [_colorTree setObject:@"e0ffff" forKey:@"lightcyan"];
-        [_colorTree setObject:@"fafad2" forKey:@"lightgoldenrodyellow"];
-        [_colorTree setObject:@"d3d3d3" forKey:@"lightgray"];
-        [_colorTree setObject:@"90ee90" forKey:@"lightgreen"];
-        [_colorTree setObject:@"d3d3d3" forKey:@"lightgrey"];
-        [_colorTree setObject:@"ffb6c1" forKey:@"lightpink"];
-        [_colorTree setObject:@"ffa07a" forKey:@"lightsalmon"];
-        [_colorTree setObject:@"20b2aa" forKey:@"lightseagreen"];
-        [_colorTree setObject:@"87cefa" forKey:@"lightskyblue"];
-        [_colorTree setObject:@"778899" forKey:@"lightslategray"];
-        [_colorTree setObject:@"778899" forKey:@"lightslategrey"];
-        [_colorTree setObject:@"b0c4de" forKey:@"lightsteelblue"];
-        [_colorTree setObject:@"ffffe0" forKey:@"lightyellow"];
-        [_colorTree setObject:@"00ff00" forKey:@"lime"];
-        [_colorTree setObject:@"32cd32" forKey:@"limegreen"];
-        [_colorTree setObject:@"faf0e6" forKey:@"linen"];
-        [_colorTree setObject:@"ff00ff" forKey:@"magenta"];
-        [_colorTree setObject:@"800000" forKey:@"maroon"];
-        [_colorTree setObject:@"66cdaa" forKey:@"mediumaquamarine"];
-        [_colorTree setObject:@"0000cd" forKey:@"mediumblue"];
-        [_colorTree setObject:@"ba55d3" forKey:@"mediumorchid"];
-        [_colorTree setObject:@"9370db" forKey:@"mediumpurple"];
-        [_colorTree setObject:@"3cb371" forKey:@"mediumseagreen"];
-        [_colorTree setObject:@"7b68ee" forKey:@"mediumslateblue"];
-        [_colorTree setObject:@"00fa9a" forKey:@"mediumspringgreen"];
-        [_colorTree setObject:@"48d1cc" forKey:@"mediumturquoise"];
-        [_colorTree setObject:@"c71585" forKey:@"mediumvioletred"];
-        [_colorTree setObject:@"191970" forKey:@"midnightblue"];
-        [_colorTree setObject:@"f5fffa" forKey:@"mintcream"];
-        [_colorTree setObject:@"ffe4e1" forKey:@"mistyrose"];
-        [_colorTree setObject:@"ffe4b5" forKey:@"moccasin"];
-        [_colorTree setObject:@"ffdead" forKey:@"navajowhite"];
-        [_colorTree setObject:@"000080" forKey:@"navy"];
-        [_colorTree setObject:@"fdf5e6" forKey:@"oldlace"];
-        [_colorTree setObject:@"808000" forKey:@"olive"];
-        [_colorTree setObject:@"6b8e23" forKey:@"olivedrab"];
-        [_colorTree setObject:@"ffa500" forKey:@"orange"];
-        [_colorTree setObject:@"ff4500" forKey:@"orangered"];
-        [_colorTree setObject:@"da70d6" forKey:@"orchid"];
-        [_colorTree setObject:@"eee8aa" forKey:@"palegoldenrod"];
-        [_colorTree setObject:@"98fb98" forKey:@"palegreen"];
-        [_colorTree setObject:@"afeeee" forKey:@"paleturquoise"];
-        [_colorTree setObject:@"db7093" forKey:@"palevioletred"];
-        [_colorTree setObject:@"ffefd5" forKey:@"papayawhip"];
-        [_colorTree setObject:@"ffdab9" forKey:@"peachpuff"];
-        [_colorTree setObject:@"cd853f" forKey:@"peru"];
-        [_colorTree setObject:@"ffc0cb" forKey:@"pink"];
-        [_colorTree setObject:@"dda0dd" forKey:@"plum"];
-        [_colorTree setObject:@"b0e0e6" forKey:@"powderblue"];
-        [_colorTree setObject:@"800080" forKey:@"purple"];
-        [_colorTree setObject:@"ff0000" forKey:@"red"];
-        [_colorTree setObject:@"bc8f8f" forKey:@"rosybrown"];
-        [_colorTree setObject:@"4169e1" forKey:@"royalblue"];
-        [_colorTree setObject:@"8b4513" forKey:@"saddlebrown"];
-        [_colorTree setObject:@"fa8072" forKey:@"salmon"];
-        [_colorTree setObject:@"f4a460" forKey:@"sandybrown"];
-        [_colorTree setObject:@"2e8b57" forKey:@"seagreen"];
-        [_colorTree setObject:@"fff5ee" forKey:@"seashell"];
-        [_colorTree setObject:@"a0522d" forKey:@"sienna"];
-        [_colorTree setObject:@"c0c0c0" forKey:@"silver"];
-        [_colorTree setObject:@"87ceeb" forKey:@"skyblue"];
-        [_colorTree setObject:@"6a5acd" forKey:@"slateblue"];
-        [_colorTree setObject:@"708090" forKey:@"slategray"];
-        [_colorTree setObject:@"708090" forKey:@"slategrey"];
-        [_colorTree setObject:@"fffafa" forKey:@"snow"];
-        [_colorTree setObject:@"00ff7f" forKey:@"springgreen"];
-        [_colorTree setObject:@"4682b4" forKey:@"steelblue"];
-        [_colorTree setObject:@"d2b48c" forKey:@"tan"];
-        [_colorTree setObject:@"008080" forKey:@"teal"];
-        [_colorTree setObject:@"d8bfd8" forKey:@"thistle"];
-        [_colorTree setObject:@"ff6347" forKey:@"tomato"];
-        [_colorTree setObject:@"40e0d0" forKey:@"turquoise"];
-        [_colorTree setObject:@"ee82ee" forKey:@"violet"];
-        [_colorTree setObject:@"f5deb3" forKey:@"wheat"];
-        [_colorTree setObject:@"ffffff" forKey:@"white"];
-        [_colorTree setObject:@"f5f5f5" forKey:@"whitesmoke"];
-        [_colorTree setObject:@"ffff00" forKey:@"yellow"];
-        [_colorTree setObject:@"9acd32" forKey:@"yellowgreen"];
+        _colorTree = [@{
+            @"aliceblue":@"f0f8ff",
+            @"antiquewhite":@"faebd7",
+            @"aqua":@"00ffff",
+            @"aquamarine":@"7fffd4",
+            @"azure":@"f0ffff",
+            @"beige":@"f5f5dc",
+            @"bisque":@"ffe4c4",
+            @"black":@"000000",
+            @"blanchedalmond":@"ffebcd",
+            @"blue":@"0000ff",
+            @"blueviolet":@"8a2be2",
+            @"brown":@"a52a2a",
+            @"burlywood":@"deb887",
+            @"cadetblue":@"5f9ea0",
+            @"chartreuse":@"7fff00",
+            @"chocolate":@"d2691e",
+            @"coral":@"ff7f50",
+            @"cornflowerblue":@"6495ed",
+            @"cornsilk":@"fff8dc",
+            @"crimson":@"dc143c",
+            @"currentcolor":@"000000",
+            @"cyan":@"00ffff",
+            @"darkblue":@"00008b",
+            @"darkcyan":@"008b8b",
+            @"darkgoldenrod":@"b8860b",
+            @"darkgray":@"a9a9a9",
+            @"darkgreen":@"006400",
+            @"darkgrey":@"a9a9a9",
+            @"darkkhaki":@"bdb76b",
+            @"darkmagenta":@"8b008b",
+            @"darkolivegreen":@"556b2f",
+            @"darkorange":@"ff8c00",
+            @"darkorchid":@"9932cc",
+            @"darkred":@"8b0000",
+            @"darksalmon":@"e9967a",
+            @"darkseagreen":@"8fbc8f",
+            @"darkslateblue":@"483d8b",
+            @"darkslategray":@"2f4f4f",
+            @"darkslategrey":@"2f4f4f",
+            @"darkturquoise":@"00ced1",
+            @"darkviolet":@"9400d3",
+            @"deeppink":@"ff1493",
+            @"deepskyblue":@"00bfff",
+            @"dimgray":@"696969",
+            @"dimgrey":@"696969",
+            @"dodgerblue":@"1e90ff",
+            @"firebrick":@"b22222",
+            @"floralwhite":@"fffaf0",
+            @"forestgreen":@"228b22",
+            @"fuchsia":@"ff00ff",
+            @"gainsboro":@"dcdcdc",
+            @"ghostwhite":@"f8f8ff",
+            @"gold":@"ffd700",
+            @"goldenrod":@"daa520",
+            @"gray":@"808080",
+            @"green":@"008000",
+            @"greenyellow":@"adff2f",
+            @"grey":@"808080",
+            @"honeydew":@"f0fff0",
+            @"hotpink":@"ff69b4",
+            @"indianred":@"cd5c5c",
+            @"indigo":@"4b0082",
+            @"ivory":@"fffff0",
+            @"khaki":@"f0e68c",
+            @"lavender":@"e6e6fa",
+            @"lavenderblush":@"fff0f5",
+            @"lawngreen":@"7cfc00",
+            @"lemonchiffon":@"fffacd",
+            @"lightblue":@"add8e6",
+            @"lightcoral":@"f08080",
+            @"lightcyan":@"e0ffff",
+            @"lightgoldenrodyellow":@"fafad2",
+            @"lightgray":@"d3d3d3",
+            @"lightgreen":@"90ee90",
+            @"lightgrey":@"d3d3d3",
+            @"lightpink":@"ffb6c1",
+            @"lightsalmon":@"ffa07a",
+            @"lightseagreen":@"20b2aa",
+            @"lightskyblue":@"87cefa",
+            @"lightslategray":@"778899",
+            @"lightslategrey":@"778899",
+            @"lightsteelblue":@"b0c4de",
+            @"lightyellow":@"ffffe0",
+            @"lime":@"00ff00",
+            @"limegreen":@"32cd32",
+            @"linen":@"faf0e6",
+            @"magenta":@"ff00ff",
+            @"maroon":@"800000",
+            @"mediumaquamarine":@"66cdaa",
+            @"mediumblue":@"0000cd",
+            @"mediumorchid":@"ba55d3",
+            @"mediumpurple":@"9370db",
+            @"mediumseagreen":@"3cb371",
+            @"mediumslateblue":@"7b68ee",
+            @"mediumspringgreen":@"00fa9a",
+            @"mediumturquoise":@"48d1cc",
+            @"mediumvioletred":@"c71585",
+            @"midnightblue":@"191970",
+            @"mintcream":@"f5fffa",
+            @"mistyrose":@"ffe4e1",
+            @"moccasin":@"ffe4b5",
+            @"navajowhite":@"ffdead",
+            @"navy":@"000080",
+            @"oldlace":@"fdf5e6",
+            @"olive":@"808000",
+            @"olivedrab":@"6b8e23",
+            @"orange":@"ffa500",
+            @"orangered":@"ff4500",
+            @"orchid":@"da70d6",
+            @"palegoldenrod":@"eee8aa",
+            @"palegreen":@"98fb98",
+            @"paleturquoise":@"afeeee",
+            @"palevioletred":@"db7093",
+            @"papayawhip":@"ffefd5",
+            @"peachpuff":@"ffdab9",
+            @"peru":@"cd853f",
+            @"pink":@"ffc0cb",
+            @"plum":@"dda0dd",
+            @"powderblue":@"b0e0e6",
+            @"purple":@"800080",
+            @"red":@"ff0000",
+            @"rosybrown":@"bc8f8f",
+            @"royalblue":@"4169e1",
+            @"saddlebrown":@"8b4513",
+            @"salmon":@"fa8072",
+            @"sandybrown":@"f4a460",
+            @"seagreen":@"2e8b57",
+            @"seashell":@"fff5ee",
+            @"sienna":@"a0522d",
+            @"silver":@"c0c0c0",
+            @"skyblue":@"87ceeb",
+            @"slateblue":@"6a5acd",
+            @"slategray":@"708090",
+            @"slategrey":@"708090",
+            @"snow":@"fffafa",
+            @"springgreen":@"00ff7f",
+            @"steelblue":@"4682b4",
+            @"tan":@"d2b48c",
+            @"teal":@"008080",
+            @"thistle":@"d8bfd8",
+            @"tomato":@"ff6347",
+            @"turquoise":@"40e0d0",
+            @"violet":@"ee82ee",
+            @"wheat":@"f5deb3",
+            @"white":@"ffffff",
+            @"whitesmoke":@"f5f5f5",
+            @"yellow":@"ffff00",
+            @"yellowgreen":@"9acd32"
+        } retain];
     });
 }
 
