2f0bead7a416b9f7e44b64497896323cc41fddf2
Curtis Hard <curtishard@me.com>
2019-06-10 15:10:52 +0100

    Sets initial stroke width to 1.0, should not be 0.f!

--- source/IJSVGNode.m
+++ source/IJSVGNode.m
@@ -223,7 +223,7 @@
         self.strokeOpacity = [IJSVGUnitLength unitWithFloat:1.f];
         self.strokeOpacity.inherit = YES;
         
-        self.strokeWidth = [IJSVGUnitLength unitWithFloat:0.f];
+        self.strokeWidth = [IJSVGUnitLength unitWithFloat:1.f];
         self.strokeWidth.inherit = YES;
         
         self.windingRule = IJSVGWindingRuleInherit;
